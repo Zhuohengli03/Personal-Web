9636ce985f163f48f420b6da19b117ea68fb75a8
Zhuohengli03 <zhuohengli03@gmail.com>
2026-02-04 17:56:35 -0500

    Optimize effects

--- astro-portfolio/public/js/main.js
+++ astro-portfolio/public/js/main.js
@@ -5,11 +5,3 @@ const nav = document.querySelector(".nav");
 const toggle = document.querySelector(".nav-toggle");
 if (toggle) toggle.addEventListener("click", () => nav?.classList.toggle("open"));
 document.querySelectorAll(".nav-links a").forEach((a) => a.addEventListener("click", () => nav?.classList.remove("open")));
-
-document.addEventListener("click", (e) => {
-  const back = e.target.closest?.("[data-back]");
-  if (back) {
-    e.preventDefault();
-    history.back();
-  }
-});
